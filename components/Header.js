77e2de68f8137a37e514943d1db1ea31668d49e2
neginkou <negin.kou@gmail.com>
2024-02-27 21:16:00 -0800

    added components and modified index.js

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+    <>
+        <div className="w-full"></div>
+        <header className="flex items-center h-12 pt-8 pb-8 pl-4 text-4xl bg-green-500">
+        <h1>Cookie Stand Admin</h1>
+        </header>
+    </>
+    )
+  }
